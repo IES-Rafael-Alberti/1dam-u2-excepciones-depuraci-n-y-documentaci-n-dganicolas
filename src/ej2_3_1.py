38e1362b0507fcedc4a1cabdf867eb1168245803
amahcan562 <amahcan562@g.educaand.es>
2023-11-07 19:50:34 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/ej2_3_1.py
+++ src/ej2_3_1.py
@@ -1,18 +1,22 @@
 #Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
-def frase_edades(edad:str):
-    try:
-        edad= "\n"+ "has cumplido " +str(edad) + " años."
-    except ValueError as e:
-        if edad == str:
-            print("La edad debe de ser un numero")
-        else:
-            print(e)
+def frase_edades(edad):
+    edad= "\n"+ "has cumplido " +str(edad) + " años."
     return edad
 
 def pregunta_edad():
-    años= int(input("dime tu edad: "))
-    return años
+    
+    try:
+        años= input("dime tu edad: ")
+    
+        if años == str:
+            print()
+    except ValueError:
+        print("La edad debe de ser un numero")
+            
+        años =int(años)
+        return años
 def edades (edad):
+    edad = edad
     edad_texto = 0
     serie_edad= "has cumplido " +str(edad_texto) + " años."
     cont=0
@@ -21,6 +25,7 @@ def edades (edad):
         serie_edad+=frase_edades(edad_texto)
         cont+=1
     return(serie_edad)
+    
 
 def main():
     años = pregunta_edad()
